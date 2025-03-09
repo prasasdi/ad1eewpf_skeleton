d9b4a726cd9340367f0c98991d2055bda1f98cf5
prasasdi <rizky.adie7@gmail.com>
2025-03-10 06:22:50 +0700

    Add project files.

---
+++ DependencyInjection/RegisterDependencies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelajarNotifyPropertyChange.DependencyInjection
+{
+    public class RegisterDependencies
+    {
+    }
+}
---
+++ FooModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelajarNotifyPropertyChange
+{
+    public class FooModel : INotifyPropertyChanged
+    {
+        private string name;
+        
+        //Declare event
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public FooModel() {}
+
+        public FooModel(string name) 
+        {
+            this.name = name;
+        }
+
+        // getter setter
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
